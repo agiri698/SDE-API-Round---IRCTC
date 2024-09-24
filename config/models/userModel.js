b9d1f0402bc68bc8a45e428078b7711b287e8d84
agiri698 <109633727+agiri698@users.noreply.github.com>
2024-09-25 02:34:06 +0530

    Create userModel.js

---
+++ config/models/userModel.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  username: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, defaultValue: 'user' }  // Admin/User
+});
+
+module.exports = User;
